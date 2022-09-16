841c1d13c93d3b249749701caa4646c01c567c9b
Mathis Pinsault <pinsaultm@gmail.com>
2022-09-17 00:48:44 +0200

    test action

--- src/weeklybot.ts
+++ src/weeklybot.ts
@@ -1,5 +1,6 @@
 // Require the Node Slack SDK package (github.com/slackapi/node-slack-sdk)
 import { App, Logger, LogLevel, } from "@slack/bolt"
+import { DateTime } from "luxon"
 
 import dotenv from "dotenv"
 dotenv.config()
@@ -8,28 +9,10 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   logLevel: LogLevel.DEBUG,
 });
-
 function getNextMondayMorning(logger: Logger) {
-  const nowParis = new Date(new Date().toLocaleString("en-US", { timeZone: "Europe/Paris" }));
-  // Get the current date in Europe/Paris timezone
-  const now = new Date();
-  const currentDay = now.getDay();
-  const currentHour = now.getHours();
-  // Get the next Monday morning
-  const nextMondayMorning = new Date();
-  nextMondayMorning.setDate(now.getDate() + (8 - currentDay) % 7);
-  nextMondayMorning.setHours(9 + nowParis.getTimezoneOffset());
-  nextMondayMorning.setMinutes(0);
-  nextMondayMorning.setSeconds(0);
-  nextMondayMorning.setMilliseconds(0);
-  // If it's already Monday morning, add 7 days
-  if (currentDay === 1 && currentHour >= 9) {
-    nextMondayMorning.setDate(nextMondayMorning.getDate() + 7);
-  }
-
-  logger.info(`Next Monday morning is ${nextMondayMorning} ofset is ${nowParis.getTimezoneOffset()}`);
-  // Return the date in unix timestamp
-  return nextMondayMorning.getTime() / 1000;
+  const now = DateTime.now();
+  const weekOffset = (now.weekday === 1 && now.hour <= 9 && now.minute < 30) ? 0 : 1;
+  return now.setZone("Europe/Paris").plus({ weeks: weekOffset }).startOf("week").set({ hour: 30, minute: 0, second: 0, millisecond: 0 })
 }
 
 app.action("weekly", async ({ ack, say, body, respond }) => {
@@ -109,7 +92,7 @@ app.command("/weekly", async ({ command, ack, say, logger }) => {
   await app.client.chat.scheduleMessage({
     channel: command.channel_id,
     // next monday at 9:30am CET
-    post_at: nextMonday,
+    post_at: nextMonday.toUnixInteger(),
     text: "Hello there :wave:\nIt's monday, time to pick your scribe and ambassador for the week :tada:",
     blocks: weeklyMessage.blocks
   });
@@ -121,7 +104,7 @@ app.command("/weekly-off", async ({ command, ack, say }) => {
   await ack();
   // get scheduled messages
   const { scheduled_messages } = await app.client.chat.scheduledMessages.list({
-    oldest: Date.now() / 1000,
+    oldest: DateTime.now().toUnixInteger(),
     channel: command.channel_id,
     // end of time
     latest: 2147483647
@@ -144,7 +127,7 @@ app.command("/weekly-status", async ({ command, ack, say }) => {
   await ack();
   // get scheduled messages
   const { scheduled_messages } = await app.client.chat.scheduledMessages.list({
-    oldest: Date.now() / 1000,
+    oldest: DateTime.now().toUnixInteger(),
     channel: command.channel_id,
     // end of time in unix time ms
     latest: 2147483647
@@ -152,19 +135,21 @@ app.command("/weekly-status", async ({ command, ack, say }) => {
 
   if (scheduled_messages?.length) {
     const scheduledDates = scheduled_messages.map((message) => {
-      const date = new Date((message.post_at ?? 0) * 1000);
-      return date.toLocaleDateString("en-GB", {
+      return DateTime.fromSeconds(message.post_at!).toLocaleString({
+        timeZone: "Europe/Paris",
         weekday: "long",
-        year: "numeric",
         month: "long",
-        day: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-        timeZone: "Europe/Paris"
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false
       });
-    }).join("\n");
-
-    await say("I'm still posting the message every monday at 9:30am CET :tada:\n" + scheduledDates);
+    }).slice(0, 5).join("\n");
+    if (scheduled_messages.length > 5) {
+      await say(`I'll post the message every monday at 9:30am CET :tada:\nNext messages are scheduled for:\n${scheduledDates}\n...`);
+    } else {
+      await say("I'm still posting the message every monday at 9:30am CET :tada:\n" + scheduledDates);
+    }
   } else {
     await say("I haven't scheduled any message :dotted_line_face:");
   }
